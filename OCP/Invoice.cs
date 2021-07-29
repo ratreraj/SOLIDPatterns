f355ad80ce8993ff9855912b61e626478ecf5468
raj.ratre <raj.ratre@KCDESK-GR150>
2021-07-29 09:35:18 +0530

    OCP

---
+++ OCP/Invoice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP
+{
+    public class Invoice
+    {
+
+        public double GetInvoiceDiscount(double amount, InvoiceType invoiceType)
+        {
+            double DiscoutAmount = 0;
+
+            if (invoiceType == InvoiceType.FinalInvoice)
+            {
+                DiscoutAmount = amount - 100;
+            }
+            else if (invoiceType == InvoiceType.ProposedInvoice)
+            {
+                DiscoutAmount = amount - 120;
+            }
+            return DiscoutAmount;
+
+        }
+
+        //static void Main(string[] args)
+        //{
+        //}
+
+        public enum InvoiceType
+        {
+            FinalInvoice,
+            ProposedInvoice
+
+        }
+    }
+
+
+}
---
+++ OCP/Invoices.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP
+{
+    public class Invoices
+    {
+        public virtual double GetInvoiceDiscount(double amount)
+        {
+            double DiscountAmount = 0;
+
+            DiscountAmount = amount - 100;
+
+            return DiscountAmount;
+
+        }
+    }
+
+    public class FinalInvoice : Invoices
+    {
+        public override double GetInvoiceDiscount(double amount)
+        {
+            return base.GetInvoiceDiscount(amount) - 50;
+        }
+
+    }
+    public class ProposedInvoice : Invoices
+    {
+        public override double GetInvoiceDiscount(double amount)
+        {
+            return base.GetInvoiceDiscount(amount) - 80;
+        }
+
+    }
+}
